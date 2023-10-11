b933711c9019e7ebc3c78a241cc53f54c273879a
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-10-11 12:02:43 +0900

    :sparkles: 식당 이름, 주소 입력 별점 제작중

--- src/Food_Log/Review.java
+++ src/Food_Log/Review.java
@@ -1,15 +1,28 @@
 package Food_Log;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Review extends JFrame {	
 	private ImageIcon Back = new ImageIcon("./img/Icon_Back.png");
+	private ImageIcon img_star = new ImageIcon("./img/star.png");
+	
+	private JPanel review;
+	private JLabel name;
+	private JTextField f_name;
+	private JLabel star;
+	private JLabel address;
+	private JTextField f_address;
 
 	Review() {
 		setTitle("리뷰 작성하기");
@@ -19,16 +32,65 @@ public class Review extends JFrame {
 		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setBackground(Color.white);
-
+		
+		review();
 		btnBack();
-
+		
 		setVisible(true);
+	}
+	
+	public void review() {
+		review = new JPanel();
+		review.setBounds(350, 0, 550, 600);
+		review.setLayout(null);
+		
+		//식당 이름
+		name = new JLabel("식당 이름 ");
+		name.setBounds(10, 20, 300, 50);
+		name.setFont(new Font("EF_watermelonSalad", Font.PLAIN, 25));
+		f_name = new JTextField();
+		f_name.setBounds(130, 20, 400, 45);
+		f_name.setFont(new Font("EF_watermelonSalad", Font.PLAIN, 25));
+		
+		//식당 사진
+		
+		//식당 별점
+		star = new JLabel("별점");
+		star.setBounds(30, 100, 100, 30);
+		star.setFont(new Font("EF_watermelonSalad", Font.PLAIN, 25));
+//		JPanel sp = new JPanel();
+//		sp.setLayout(new GridLayout(1,5));
+//		JButton[] star = new JButton[5];
+//		//int x = 100;
+//		for(int i =0; i<star.length; i++) {
+//			star[i].setBounds(100, 100, 50, 50);
+//			star[i].setBackground(Color.white);
+//			star[i].setFocusPainted(false);
+//			star[i].setBorderPainted(false);
+//		}
+		
+		//주소
+		address = new JLabel("주소");
+		address.setBounds(30, 150, 300, 50);
+		address.setFont(new Font("EF_watermelonSalad", Font.PLAIN, 25));
+		f_address = new JTextField();
+		f_address.setBounds(130, 150, 400, 45);
+		f_address.setFont(new Font("EF_watermelonSalad", Font.PLAIN, 25));
+		
+		
+		
+		review.add(name);
+		review.add(f_name);
+		review.add(star);
+		review.add(address);
+		review.add(f_address);
+		add(review);
 	}
 
 	//뒤로가기 버튼
 	public void btnBack() {
 		JButton btnBack = new JButton(Back);
-		btnBack.setBounds(20, 20, 70, 70);
+		btnBack.setBounds(20, 20, 50, 50);
 		btnBack.setBackground(Color.white);
 		btnBack.setFocusPainted(false);
 		btnBack.setBorderPainted(false);
